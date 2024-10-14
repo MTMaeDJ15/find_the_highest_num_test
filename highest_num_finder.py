dd3ee558775cd47a84fb12bd07cf8208f6cdd1d3
unknown <mariateresamaedj19@gmail.com>
2024-10-15 03:51:56 +0800

    Program running

--- highest_num_finder.py
+++ highest_num_finder.py
@@ -20,3 +20,16 @@ def find_the_highest(var1, var2, var3, var4, var5):
         highest_number = var5
 
     return highest_number
+
+#Let the user enter the digits. 
+print("Input Numbers ONLY!!")
+var1 = int(input("Input num1: "))
+var2 = int(input("Input num2: "))
+var3 = int(input("Input num3: "))
+var4 = int(input("Input num4: "))
+var5 = int(input("Input num5: "))
+
+#Now that the numbers are entered, we can now view the result.
+res = find_the_highest(var1,var2,var3,var4,var5)
+print(f"The Highest Number is {res}")
+
